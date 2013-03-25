e2b5e392525850b8dfac9cd04fe8251f007cdd41
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-03-25 18:04:11 +0000

    Fix the boxgrinder appliance postinstall
    Spent an entire day tracking down a broken libguestfs. Finally established that
    the virtio_console.ko kernel module was missing, due to a missing dependency
    from libguestfs on linux-image-extra-virtual.

--- script/postinstall-boxgrinder.sh
+++ script/postinstall-boxgrinder.sh
@@ -1,19 +1,33 @@
 #!/bin/sh
-# Affected by this http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=645119
+set -e
+
 export DEBIAN_FRONTEND=noninteractive 
 export DEBIAN_PRIORITY=critical 
+
+# Install boxgrinder and boxgrinder-ubuntu-plugin
 apt-add-repository -y ppa:rubiojr/boxgrinder-stable
 apt-get update
-apt-get -y install rubygems boxgrinder-build
-wget --quiet https://gds-boxes.s3.amazonaws.com/resources/guestfs-1.14.2.gem 
-gem install --no-ri --no-rdoc guestfs-1.14.2.gem 
-gem update boxgrinder-core boxgrinder-build boxgrinder-ubuntu-plugin multidisk-boxgrinder-plugin
-rm -f *.gem
-echo "Installing Boxgrinder..."
-gem install --no-ri --no-rdoc boxgrinder-build boxgrinder-ubuntu-plugin 
+# libguestfs has a missing dependency on linux-image-extra-*, which provides
+# many of the modules for the default ubuntu kernel, including
+# virtio_console.ko and friends which are required for sthe supermin appliance
+apt-get -y install linux-image-extra-virtual rubygems boxgrinder-build
 apt-get clean
 apt-get autoremove --purge
-cd /root && git clone https://github.com/alphagov/boxgrinder-appliances
+
+# boxgrinder-build is a bit broken on Ubuntu: it relies on a wrapper script
+# ("qemu.wrapper") to call out to qemu, which is a bash script with a
+# "#!/bin/sh" shebang line. Until my pull request,
+#
+#   https://github.com/boxgrinder/boxgrinder-build/pull/10 
+# 
+# is merged, we munge the file to force execution with bash.
+sed -i.bak -e 's|#!/bin/sh|#!/bin/bash|' /usr/lib/ruby/vendor_ruby/boxgrinder-build/helpers/qemu.wrapper
+chmod +x /usr/lib/ruby/vendor_ruby/boxgrinder-build/helpers/qemu.wrapper
+
+# Install VMware ovftool
 wget --quiet https://gds-boxes.s3.amazonaws.com/resources/VMware-ovftool-3.0.1-801290-lin.x86_64.bundle
 chmod +x ./VMware-ovftool-3.0.1-801290-lin.x86_64.bundle
 yes | ./VMware-ovftool-3.0.1-801290-lin.x86_64.bundle --required --console
+
+# Lastly, check out the GDS appliances
+cd /root && git clone https://github.com/alphagov/boxgrinder-appliances
