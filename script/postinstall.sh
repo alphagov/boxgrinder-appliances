af099b272375b0fda5d254b3fabc607e10f0f014
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-04-05 11:19:35 +0100

    Configure DNS servers correctly via resolvconf

--- script/postinstall.sh
+++ script/postinstall.sh
@@ -26,9 +26,11 @@ EOM
 chmod +x /etc/rc.local
 
 # Set up some sensible default nameservers
-cat >/etc/resolv.conf <<EOM
+cat >/etc/resolvconf/resolv.conf.d/original <<EOM
+# To change this, see /etc/resolvconf/resolv.conf.d/original
 nameserver 8.8.8.8
 nameserver 8.8.4.4
+search localdomain
 EOM
 
 # And, finally, truncate any and all log files
