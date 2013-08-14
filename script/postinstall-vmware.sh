60550e5be0f2720d8c7d25ad894b6935ea2e69d1
Dan Carley <dan.carley@gmail.com>
2013-08-14 14:22:01 +0100

    [#55141908] Use official packages for VMware Tools
    The open source `open-vm-tools` package doesn't allow Guest Customization on
    vCloud 5.x - the flag gets disabled when an OVA is uploaded and the checkbox
    remains greyed out in the UI, for both the template and any subsequently
    provisioned VMs.
    
    This means that newly provisioned VMs (from `vcloud-box-spinner` which uses
    instantiate vApp from template) don't set their hostname and static IP
    addresses.
    
    The official packages do support customization, to some extent. It doesn't
    seem to fix the issue of the option being disabled in the template or newly
    provisioned VM. However when a new VM is shutdown, the option can be
    changed, and the hostname/network get set correctly on the next boot.
    
    It may be that we need to change `vcloud-box-spinner` to cycle the power on
    a new VM before apply any customisations.
    
    http://www.vmware.com/download/packages.html
    
    I wasn't able to get this working on the `vmware` platform-specific
    postinstall section. It was bombing out on some libguestfs related errors, I
    think -- sorry I've since lost the errors and it takes ages to reproduce. As
    a result I've just tacked this onto the base section.

--- script/postinstall-vmware.sh
+++ script/postinstall-vmware.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 export DEBIAN_FRONTEND=noninteractive
 export DEBIAN_PRIORITY=critical
+
+echo "deb http://packages.vmware.com/tools/esx/5.1latest/ubuntu precise main" > /etc/apt/sources.list.d/vmware-tools.list
+wget http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub -q -O- | apt-key add -
+
 apt-get update
-apt-get -u install linux-headers-virtual open-vm-dkms open-vm-tools
+apt-get install -y vmware-tools-esx-nox
